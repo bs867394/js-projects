1b64820bbc16c207095faf25abaeff2f11ab1ec8
bs867394 <141112563+bs867394@users.noreply.github.com>
2024-05-31 10:09:32 +0000

    basic config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "bs@google.com"
+var accountPassword = '12345'
+accountCity = 'jaipur'
+
+
+// accountId = 2 not allowed
+/*prefer not to use var issue of block scope {} functional scope */
+
+accountEmai = ''
+ console.log('accountId')
+
+ console.table(accountEmail,accountId,accountPassword,accountCity)
