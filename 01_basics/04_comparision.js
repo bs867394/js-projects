a6f77882f9be2e6ab8bea92873c37cec772bf611
bs867394 <141112563+bs867394@users.noreply.github.com>
2024-06-01 12:03:18 +0000

    datatypes config

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -5,5 +5,19 @@
 //console.log(2!=1);
 
 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);  // not preferred
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === checks datatypes as well
+
+console.log("2" === 2);
 
 
